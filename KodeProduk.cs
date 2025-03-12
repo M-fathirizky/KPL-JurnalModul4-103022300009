f264236699120331fdf4d6b96b2cc47058617439
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 07:47:09 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod4
+{
+   
+
+class KodeProduk
+    {
+        private Dictionary<string, string> kodeProduk = new Dictionary<string, string>()
+        {
+            {"laptop", "E100" },
+            {"smartphone", "E101" },
+            {"tablet", "E102" },
+            {"Headset", "E103" },
+            {"Keyboard", "E104" },
+            {"Mouse", "E105" },
+            {"Printer", "E106" },
+            {"Monitor", "E107" },
+            {"Smartwatch", "E108" },
+            {"Kamera", "E109" }
+        };
+
+        public string GetKodeProduk(string namaProduk)
+        {
+            return kodeProduk.ContainsKey(namaProduk) ? kodeProduk[namaProduk] : "Produk tidak ditemukan";
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using mod4;
+
+namespace mod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodeProduk kodeProduk = new KodeProduk();
+            Console.WriteLine(kodeProduk.GetKodeProduk("laptop"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("smartphone"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("tablet"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Headset"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Keyboard"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Mouse"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Printer"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Monitor"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Smartwatch"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Kamera"));
+            Console.WriteLine(kodeProduk.GetKodeProduk("Speaker"));
+            
+
+        }
+    }
+}
+
+
